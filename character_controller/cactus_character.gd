f98f235dba9ec9d63af6bcaf16ab18dfef59fbd8
ABI <liam.elliott.nz@gmail.com>
2025-09-11 00:59:46 +1200

    slope math and surface snapping
    character now snaps to surfaces and transfers momentum better when landing on slopes and prevents bouncing when on downslopes with convex geometry

--- character_controller/cactus_character.gd
+++ character_controller/cactus_character.gd
@@ -3,14 +3,16 @@ extends CharacterBody2D
 enum movementMode { ROLL, SPINY }
 
 const GRAVITY: Vector2i = Vector2i(0.0, -650.0)
-const ACCELERATION: float = 600.0
-const MAX_VELOCITY: float = 500.0
+const ACCELERATION: float = 700.0
+const MAX_VELOCITY: float = 800.0
 const JUMP_STRENGTH: float = 300.0
 const AIR_CONTROL_FACTOR: float = 0.4
 const GROUND_FRICTION: float = 100.0
+const UPSIDE_DOWN_STICK_THRESHOLD: float = 550.0
 
 var move_state: movementMode = movementMode.ROLL
 var is_grounded: bool = false
+var is_jumping: bool = false
 
 var circumference: float
 var ang_velocity: float
@@ -18,6 +20,8 @@ const ANG_FRICTION: float = PI/16
 
 @onready var body = $RollingCollisionBody
 @onready var sprite = $Sprite2D
+@onready var sensor = $GroundSensorCast
+@onready var snap_sensor = $AirborneSensorCast
 
 
 func _ready() -> void:
@@ -37,42 +41,77 @@ func _physics_process(delta: float) -> void:
 		
 	velocity = velocity.clampf(-MAX_VELOCITY, MAX_VELOCITY)
 	
-	if velocity_direction.is_zero_approx() && is_grounded:
+	if velocity_direction.is_zero_approx() and is_grounded:
 		velocity += velocity.direction_to(Vector2.ZERO) * GROUND_FRICTION * delta # apply friction
-		if velocity.length() <= 6 and (up_direction.angle_to(Vector2.UP) < PI/4):
+		if velocity.length() <= 6 and (abs(up_direction.angle_to(Vector2.UP)) < PI/16): 
 			velocity = Vector2.ZERO
 		else:
 			velocity -= GRAVITY * delta
 	else:
 		velocity -= GRAVITY * delta
 	
-	sprite_roll(velocity, delta)
+	if abs(up_direction.angle_to(Vector2.DOWN)) < PI/4 and is_grounded and velocity.length() > UPSIDE_DOWN_STICK_THRESHOLD: # if ball is on an upside down surface
+		velocity += GRAVITY * delta
 	
-	if move_and_slide():
+	sprite_roll(delta)
+	
+	if move_and_slide(): #if collision occurs
 		calculate_floor_angle()
-	else:
-		is_grounded = false
-		up_direction = Vector2.UP
+	else: # no collision occurred, but check to see if we are accidentally leaving the ground
+		sensor_vector()
 	
+	$velvector.rotation = velocity.angle() + PI/2 ## TEMP
 
 
-func calculate_floor_angle() -> void:
+func calculate_floor_angle() -> void: # only called if a collision is happening
 	if is_on_floor_only():
 		up_direction = get_last_slide_collision().get_normal()
 		is_grounded = true
-	elif !is_grounded: #if in air and colliding with an object
-		if get_last_slide_collision().get_collider().get_collision_layer() == 4:
+		
+	elif !is_grounded: #if in air but contacting a surface...
+		if get_last_slide_collision().get_collider().get_collision_layer() == 4: #contacting "slippery" wall
+			floor_snap_length = 1.0
 			if get_last_slide_collision().get_normal().angle_to(Vector2.UP) < PI/8:
 				up_direction = Vector2.UP
 				is_grounded = false
-		else:
-			up_direction = get_last_slide_collision().get_normal()
+		else: 
 			is_grounded = true
-	else:
+	else: 
 		up_direction = Vector2.UP
+	
+	sensor.rotation = up_direction.angle() + PI/2
 
 
-func sprite_roll(velocity: Vector2, delta: float) -> void:
+func sensor_vector() -> void:
+	if sensor.is_colliding() and !is_jumping: 
+		# we must have disconnected from the ground on a concave curve
+		
+		up_direction = sensor.get_collision_normal()
+		apply_floor_snap()
+	elif !(sensor.is_colliding()) and is_jumping:
+		is_jumping = false
+		# we have cleared the ground by now, we can use the airborne sensor cast
+		snap_sensor.rotation = velocity.angle()
+		if velocity.x < 0:
+			snap_sensor.rotate(1.5* PI)
+		sphere_snap()
+	else: # we must either be jumping or went off an edge
+		# follow velocity for future snapping
+		up_direction = Vector2.UP
+		is_grounded = false
+		snap_sensor.rotation = velocity.angle()
+		if velocity.x < 0:
+			snap_sensor.rotate(1.5* PI)
+		sphere_snap()
+
+
+func sphere_snap() -> void:
+	if snap_sensor.is_colliding():
+		up_direction = snap_sensor.get_collision_normal()
+		print("Sphere snap!")
+		apply_floor_snap()
+
+func sprite_roll(delta: float) -> void:
 	var distance: float = velocity.length() * delta
 	if velocity.x < 0:
 		distance = distance * -1
@@ -100,3 +139,4 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("jump") and is_grounded:
 		print("jump pressed")
 		velocity += up_direction * JUMP_STRENGTH
+		is_jumping = true
