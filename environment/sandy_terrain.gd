718bf2959f84d15184ce930843d044dd8e1ca306
Nicholas O'Connor <earthenturtle@gmail.com>
2025-09-17 15:51:45 -0700

    finalize sandy terrain to use Path2D

--- environment/sandy_terrain.gd
+++ environment/sandy_terrain.gd
@@ -4,27 +4,37 @@ extends StaticBody2D
 
 @export var terrain_boundary: Path2D:
 	set(new_boundary):
+		if terrain_boundary != null:
+			terrain_boundary.property_list_changed.disconnect(_on_path_updated)
+			if terrain_boundary.curve != null:
+				terrain_boundary.curve.changed.disconnect(_on_curve_updated)
+			
 		terrain_boundary = new_boundary
-		update_configuration_warnings()
 		
 		if terrain_boundary != null:
 			terrain_boundary.property_list_changed.connect(_on_path_updated)
 			if terrain_boundary.curve != null:
 				terrain_boundary.curve.changed.connect(_on_curve_updated)
 				_update_polygons()
-
+			
+		update_configuration_warnings()
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings = []
 	
 	if terrain_boundary == null:
 		warnings.append("Terrain needs a Path2D to define its area.")
+	elif terrain_boundary.curve == null:
+		warnings.append("Path2D needs a curve defined")
 	
 	return warnings
 
 func _ready() -> void:
 	if terrain_boundary == null:
 		push_error("Terrain has no Path2D to define its area")
+		return
+	elif terrain_boundary.curve == null:
+		push_error("Terrain's Path2D needs a curve defined")
 		return
 	
 	_update_polygons()
@@ -35,9 +45,12 @@ func _update_polygons() -> void:
 	$CollisionPolygon2D.polygon = baked_points
 	
 func _on_curve_updated():
+	# don't need to update warnings because we already have a curve if this is getting called
 	_update_polygons()
-
+	
 func _on_path_updated():
 	if terrain_boundary.curve != null:
 		terrain_boundary.curve.changed.connect(_on_curve_updated)
+		_update_polygons()
 	update_configuration_warnings()
+	
